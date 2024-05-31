ae67d61e2e9af4f40e89ac3c93b55f4ffcd3244d
sebastien <sebastien.escudero@gmail.com>
2024-05-31 21:41:52 +0200

    refactor(MediaFactory): refactor MediaFactory, PhotographerMedias and add a new method "createMediaItem()" to MediaFactory

--- scripts/factories/MediasFactory.js
+++ scripts/factories/MediasFactory.js
@@ -1,15 +1,28 @@
 import Image from '../models/Image.js'
 import Video from '../models/Video.js'
-
 export default class MediasFactory {
     constructor(data) {
         this.data = data
     }
+    // Create the media depending on the media type (image or video)
     createMedia(data) {
         if (data.image) {
             return new Image(data)
-        } else {
+        }
+        if (data.video) {
             return new Video(data)
+        }
+    }
+    // Create the media card depending on the media type (image or video) and return it with the photographer name as src attribute to find the media in the right folder
+    createMediaCard(data, photographerName) {
+        const media = this.createMedia(data)
+        if (media instanceof Image) {
+            return `<img class="main__content__media__image" src="/assets/images/photographers/Sample Photos/${photographerName}/${media.image}" alt="${media.title}">`
+        }
+        if (this.createMedia(data) instanceof Video) {
+            return `<video class="main__content__media__video" controls>
+            <source src="/assets/images/photographers/Sample Photos/${photographerName}/${media.video}" type="video/mp4">
+            </video>`
         }
     }
 }
--- scripts/templates/PhotographerMedias.js
+++ scripts/templates/PhotographerMedias.js
@@ -1,40 +1,48 @@
+import MediasFactory from '../factories/MediasFactory.js'
+
 export default class PhotographerMedias {
     constructor(photographer, medias) {
         this.photographer = photographer
         this.medias = medias
     }
+
     createPhotographerMedias() {
         const content_section = document.querySelector('.main__content')
-        content_section.innerHTML += `
-        <div class="main__content__media">
-        ${this.medias
+
+        const mediaItems = this.medias
             .map((media, index) => {
-                const mediaContent = media.image
-                    ? `<img class="main__content__media__image" src="/assets/images/photographers/Sample Photos/${this.photographer.name}/${media.image}" alt="${media.title}">`
-                    : `<video class="main__content__media__video" controls>
-            <source src="/assets/images/photographers/Sample Photos/${this.photographer.name}/${media.video}" type="video/mp4">
-            </video>`
+                const mediaContent = new MediasFactory(media).createMediaCard(
+                    media,
+                    this.photographer.name
+                )
                 return `
-                <article class="main__content__media__item" data-id="${media.id}" data-order='${index}' style="order:${index}">
-                <figure data-media="${media.id}" aria-label="${media.title} closeup view" alt="${media.title}">${mediaContent}</figure>  
-                <figcaption class="main__content__media__item__caption">
-                <h2 class="main__content__media__item__caption__title">${media.title}</h2>
-                <div class="main__content__media__item__caption__likes">
-                <button class="main__content__media__item__caption__likes-btn" aria-label="likes" data-likes="${media.likes}">${media.likes}</button>
-                <i class="fas fa-heart"></i>
-                </div>
-               </figcaption>
+                <article class="main__content__media__item" data-id="${media.id}" data-order="${index}" style="order:${index}">
+                    <figure data-media="${media.id}" aria-label="${media.title} closeup view">
+                        ${mediaContent}
+                    </figure>
+                    <figcaption class="main__content__media__item__caption">
+                        <h2 class="main__content__media__item__caption__title">${media.title}</h2>
+                        <div class="main__content__media__item__caption__likes">
+                            <button class="main__content__media__item__caption__likes-btn" aria-label="likes" data-likes="${media.likes}">${media.likes}</button>
+                            <i class="fas fa-heart"></i>
+                        </div>
+                    </figcaption>
                 </article>
             `
             })
-            .join('')}
-        <aside>
-        <p class="photographer_Likes">
-            <span class="photographer_likes_count"></span>
-            <span class="fas fa-heart" aria-hidden="true"></span>
-        </p>
-        <span>${this.photographer.price}€ / jour</span>
-    </aside>
-        </div>`
+            .join('')
+
+        content_section.innerHTML += `
+            <div class="main__content__media">
+                ${mediaItems}
+                <aside>
+                    <p class="photographer_Likes">
+                        <span class="photographer_likes_count"></span>
+                        <span class="fas fa-heart" aria-hidden="true"></span>
+                    </p>
+                    <span>${this.photographer.price}€ / jour</span>
+                </aside>
+            </div>
+        `
     }
 }
